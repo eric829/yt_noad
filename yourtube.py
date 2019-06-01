10e20bbac2af9e226cd874f710bc51e6de9ee58b
eric829 <eric78829@gmail.com>
2019-06-01 16:59:50 +0800

    requerments change

--- yourtube.py
+++ yourtube.py
@@ -21,40 +21,40 @@ def url():
     'embed_config':'{}'
     }
     playlist =[]
-    try:
-        if request.method == 'POST':   
-            if "list" in request.values['res']: 
-                url = request.values['res']
-                response = requests.get(url)
-                soup = BeautifulSoup(response.text, 'html5lib')
-                #找出a,包含href
-                res2= soup.find_all('a')    
-                for i in res2:
-                    s = i.get('href')
-                #列出playlist項目
-                    if "&index" in s:
-                #拼裝成短網址查找音源檔
-                        url_emb ="https://www.youtube.com/get_video_info?html5=1&video_id="+s[9:20]
-                        res3 = requests.get(url_emb, params=params)
-                        a = urllib.unquote(urllib.unquote(res3.text))
-                        if "---sn-" in a :
-                            playlist.append(s[9:20])
-                    #移除重複項
-                    playlist2 = list(set(playlist))              
-                    playlist3 = ','.join(str(k) for k in playlist2)      
-                    url = "https://www.youtube.com/embed/VIDEO_ID?playlist=" + playlist3
-                return render_template('video.html',res=url)   
-            elif "watch?" in request.values['res']: 
-                url = "https://www.youtube.com/embed/" + request.values['res'][-11:]
-                return render_template('video.html',res=url)
-            elif "https://youtu.be/" in request.values['res']:
-                url = "https://www.youtube.com/embed/" + request.values['res'][-11:]
-                return render_template('video.html',res=url)
-            elif "embed" in request.values['res']: 
-                url = request.values['res']
-                return render_template('video.html',res=url)
-    except:
-        return render_template('url.html')
+#    try:
+    if request.method == 'POST':   
+        if "list" in request.values['res']: 
+            url = request.values['res']
+            response = requests.get(url)
+            soup = BeautifulSoup(response.text, 'html5lib')
+            #找出a,包含href
+            res2= soup.find_all('a')    
+            for i in res2:
+                s = i.get('href')
+            #列出playlist項目
+                if "&index" in s:
+            #拼裝成短網址查找音源檔
+                    url_emb ="https://www.youtube.com/get_video_info?html5=1&video_id="+s[9:20]
+                    res3 = requests.get(url_emb, params=params)
+                    a = urllib.unquote(urllib.unquote(res3.text))
+                    if "---sn-" in a :
+                        playlist.append(s[9:20])
+                #移除重複項
+                playlist2 = list(set(playlist))              
+                playlist3 = ','.join(str(k) for k in playlist2)      
+                url = "https://www.youtube.com/embed/VIDEO_ID?playlist=" + playlist3
+            return render_template('video.html',res=url)   
+        elif "watch?" in request.values['res']: 
+            url = "https://www.youtube.com/embed/" + request.values['res'][-11:]
+            return render_template('video.html',res=url)
+        elif "https://youtu.be/" in request.values['res']:
+            url = "https://www.youtube.com/embed/" + request.values['res'][-11:]
+            return render_template('video.html',res=url)
+        elif "embed" in request.values['res']: 
+            url = request.values['res']
+            return render_template('video.html',res=url)
+#    except:
+#        return render_template('url.html')
 
     return render_template('url.html')
 
